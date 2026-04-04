c10f28494cf689d3cb5503fbe861fce9edf37076
Brian McDonald <brian.mcdonald1980@googlemail.com>
2026-04-04 10:07:29 +0100

    Added in Items

---
+++ Interactables/key.gd
@@ -0,0 +1,12 @@
+@tool
+extends "res://Interactables/base_interactable.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super._ready() # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
